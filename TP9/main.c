1ae05b1698826671a42091fbc148839ea81692f7
alfigueroa1 <alfigueroa@itba.edu.ar>
2019-05-23 14:43:01 -0300

    Hago un Commit

---
+++ TP9/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: HP
+ *
+ * Created on May 23, 2019, 2:40 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Alex le esta ense;anndo a Marti como usar github
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
